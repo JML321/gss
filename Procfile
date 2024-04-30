e76750fa13000e59352524926b2ff4c3a7689d4e
Justin Max Langfan <justinlangfan@gmail.com>
2024-04-30 08:31:57 -0400

    finally got a working copy, where path is likely going to work with heroku. Moved my main python file to the root directory, changed the name of that file, changed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn code.dashboard:server
+web: gunicorn run:server
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from dash import dcc, html, Input, Output, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 import os
-from toolbox import compare_years_delta
+from app_code.toolbox import compare_years_delta
 
 print("Starting dashboard.py...")
 
@@ -90,6 +90,33 @@ app.layout = dbc.Container([
 ], fluid=True)
 
 print("Dash layout set up.")
+
+@callback(
+    Output('sub-segment-dropdown', 'options'),
+    Output('sub-segment-dropdown', 'value'),
+    Input('segment-dropdown', 'value')
+)
+def update_subsegment(segment):
+    print(f"Updating sub-segment dropdown for segment: {segment}")
+    options = sub_segment_options.get(segment, [])
+    return [{'label': opt, 'value': opt} for opt in options], options[0] if options else None
+
+@callback(
+    Output('table-container', 'children'),
+    [Input('segment-dropdown', 'value'),
+    Input('sub-segment-dropdown', 'value'),
+    Input('start-year-dropdown', 'value'),
+    Input('end-year-dropdown', 'value'),
+    Input('num-rows-input', 'value')]
+)
+def update_output(segment, sub_segment, start_year, end_year, num_rows):
+    filename = segment_files[segment]
+    print(f"Loading data from file: {filename}")
+    df = pd.read_csv(f"own_data_objects/melted_tables/{filename}")
+    print(f"Data loaded, performing year comparison for years {start_year} and {end_year}")
+    df = compare_years_delta(df, int(start_year), int(end_year))
+    return dbc.Table.from_dataframe(df.head(num_rows), striped=True, bordered=True, hover=True, className='table')
+
 
 if __name__ == '__main__':
     print("Running the server...")
